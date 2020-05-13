17a3c260f2bc0d24d8b4e272c319a6d508e6fe0f
VitorBarbosa1 <46977318+VitorBarbosa1@users.noreply.github.com>
2020-05-13 11:17:33 +0100

    Initial commit

---
+++ vitorBarbosa_S00185772/Phone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vitorBarbosa_S00185772
+{
+    public class Phone
+    {
+        public string phoneName { get; set; }
+        public int phonePrice { get; set; }
+
+        public string OsName { get; set; }
+
+        public string OsImage { get; set; }
+
+        public string phoneIamge { get; set; }
+
+
+
+    }
+}
